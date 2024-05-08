d426d7c7e9cb64348780c81a3fca1e5411bf1aa1
Darren Lindsay <daz7474@gmail.com>
2024-05-08 16:56:06 +0100

    Added new car class and console line

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ public class Program
 {
   public static void Main(string[] args)
   {
+    // Create an instance of Car
+    Car car = new Car("Lexus", "IS200", "GL02ULE", 2002, 2000m);
 
+    // Console the ToString method on car
+    Console.WriteLine(car.ToString());
   }
 }
