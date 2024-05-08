e30565a79add292e9cae8c5c4e0e0a0b67f55c87
Darren Lindsay <daz7474@gmail.com>
2024-05-08 13:52:33 +0100

    Added fields and constructor

--- Car.cs
+++ Car.cs
@@ -1,4 +1,30 @@
 public class Car
 {
+  private string make;
+  private string model;
+  private string registration;
+  private int year;
+  private decimal currentValue;
 
+  // Constructor for car
+  public Car(string make, string model, string registration, int year, decimal currentValue)
+  {
+    this.make = make;
+    this.model = model;
+    this.registration = registration;
+    this.year = year;
+    this.currentValue = currentValue;
+  }
+
+  // Get the current value of the car
+  public decimal GetCurrentValue()
+  {
+    return currentValue;
+  }
+
+  // Get the year of the car
+  public int GetYear()
+  {
+    return year;
+  }
 }
